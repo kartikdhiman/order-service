ca76a528e82f9b54b00403c3d9876367f43eeaff
kartikdhiman <59189590+kartikdhiman@users.noreply.github.com>
2024-07-09 16:09:20 +0530

    chore: update tests

--- src/test/java/com/polarbookshop/orderservice/OrderServiceApplicationTests.java
+++ src/test/java/com/polarbookshop/orderservice/OrderServiceApplicationTests.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
@@ -29,6 +31,21 @@ class OrderServiceApplicationTests {
 
 	@MockBean
 	private BookClient bookClient;
+
+	@DynamicPropertySource
+	static void postgresqlProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.r2dbc.url", OrderServiceApplicationTests::r2dbcUrl);
+		registry.add("spring.r2dbc.username", postgresql::getUsername);
+		registry.add("spring.r2dbc.password", postgresql::getPassword);
+		registry.add("spring.flyway.url", postgresql::getJdbcUrl);
+	}
+
+	private static String r2dbcUrl() {
+		return String.format("r2dbc:postgresql://%s:%s/%s",
+						postgresql.getHost(),
+						postgresql.getMappedPort(PostgreSQLContainer.POSTGRESQL_PORT),
+						postgresql.getDatabaseName());
+	}
 
 	@Test
 	void connectionEstablished() {
